fde75463db542a7b1750df697504056376850ed9
vitaliy_khodataev <khodataev.v@yandex.ru>
2020-05-13 12:07:57 +0300

    test app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,40 @@
+import sweets.*;
+import sweets.candy.*;
+import sweets.chocolate.*;
+import sweets.lollypop.*;
+import sweets.jelly.*;
+
+import java.util.ArrayList;
+
 public class App {
+
     public static void main(String[] args) {
+
+
+        ArrayList<Sweetnees> myList = new ArrayList<Sweetnees>();
+
+        myList.add(new CandyChew());
+        myList.add(new CandyChocoBar());
+        myList.add(new CandyCoco());
+        myList.add(new CandyRom());
+
+        myList.add(new ChocolateCube());
+        myList.add(new ChocolateEgg());
+
+        myList.add(new JellyCola());
+        myList.add(new JellyRandom());
+
+        myList.add(new LollypopInStick());
+        myList.add(new LollypopXXL());
+
+
+
+        for (Sweetnees sw: myList) {
+            System.out.println(sw);
+        }
+
+
+
 
     }
 }
