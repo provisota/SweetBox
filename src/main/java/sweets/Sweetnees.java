686a60d0583289cd801d4395dbee57cc486cb3d5
vitaliy_khodataev <khodataev.v@yandex.ru>
2020-05-13 08:28:53 +0300

    add Sweetnees.java and parametrs. override toString

---
+++ src/main/java/sweets/Sweetnees.java
@@ -0,0 +1,24 @@
+package sweets;
+
+public abstract class Sweetnees {
+    String title;
+    int weight; //in gramm
+    double price;
+    String unique_parameter;
+
+    protected String getInfo() {
+        String info = "";
+        info += "Сладость{" +
+                "название='" + title + '\'' +
+                ", вес=" + weight +
+                ", цена=" + price +
+                ", уникальный павраметр='" + unique_parameter + '\'' +
+                '}';
+        return info;
+    }
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+}
